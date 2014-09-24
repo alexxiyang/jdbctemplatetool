28ba79e4ca7db8262d8c4742bdbbcefcc05b5a72
alexy <alexy@L320.ehi.ehealth.com>
2014-09-24 16:38:22 +0800

    update ScaffoldUnit.sql

--- src/test/resources/ScaffoldUnit.sql
+++ src/test/resources/ScaffoldUnit.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `employee`;
 CREATE TABLE `employee` (
   `id` int(11) NOT NULL,
   `name` varchar(300) NOT NULL,
-  `join_date` datatime NOT NULL,
+  `join_date` DATETIME NOT NULL,
   `age` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
