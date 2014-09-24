519b9afea97b252ceb0c12b3a77a1fcc77a313e3
alexy <alexy@L320.ehi.ehealth.com>
2014-09-24 16:05:21 +0800

    add log4j

--- src/test/java/org/crazycake/jdbcTemplateTool/JdbcTemplateToolTest.java
+++ src/test/java/org/crazycake/jdbcTemplateTool/JdbcTemplateToolTest.java
@@ -2,12 +2,14 @@ package org.crazycake.jdbcTemplateTool;
 
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.crazycake.ScaffoldUnit.ScaffoldUnit.*;
 
 import org.crazycake.ScaffoldUnit.ScaffoldUnit;
 import org.crazycake.jdbcTemplateTool.exception.NoColumnAnnotationFoundException;
@@ -21,7 +23,8 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 public class JdbcTemplateToolTest extends AbstractJUnit4SpringContextTests{
 
 	@Test
-	public void testList() {
+	public void testList() throws IOException, SQLException {
+		build();
 		JdbcTemplateTool jtt = super.applicationContext.getBean("jdbcTemplateTool",JdbcTemplateTool.class);
 		List<Employee> es = jtt.list("select * from employee where age > ? order by id desc", new Object[]{30}, Employee.class);
 		
@@ -31,7 +34,9 @@ public class JdbcTemplateToolTest extends AbstractJUnit4SpringContextTests{
 	}
 
 	@Test
-	public void testCount() {
+	public void testCount() throws IOException, SQLException {
+		build();
+		
 		JdbcTemplateTool jtt = super.applicationContext.getBean("jdbcTemplateTool",JdbcTemplateTool.class);
 		
 		int total = jtt.count("select count(1) from employee", null);
@@ -39,7 +44,11 @@ public class JdbcTemplateToolTest extends AbstractJUnit4SpringContextTests{
 	}
 
 	@Test
-	public void testGet() throws NoIdAnnotationFoundException, NoColumnAnnotationFoundException {
+	public void testGet() throws NoIdAnnotationFoundException, NoColumnAnnotationFoundException, IOException, SQLException {
+		
+		build();
+		
+		
 		JdbcTemplateTool jtt = super.applicationContext.getBean("jdbcTemplateTool",JdbcTemplateTool.class);
 		
 		Employee e = jtt.get(Employee.class, 3);
@@ -48,6 +57,8 @@ public class JdbcTemplateToolTest extends AbstractJUnit4SpringContextTests{
 
 	@Test
 	public void testUpdate() throws Exception {
+		build();
+		
 		JdbcTemplateTool jtt = super.applicationContext.getBean("jdbcTemplateTool",JdbcTemplateTool.class);
 		
 		Employee e = jtt.get(Employee.class, 1);
@@ -57,7 +68,9 @@ public class JdbcTemplateToolTest extends AbstractJUnit4SpringContextTests{
 	}
 
 	@Test
-	public void testBatchUpdate() throws SQLException {
+	public void testBatchUpdate() throws SQLException, IOException {
+		build();
+		
 		JdbcTemplateTool jtt = super.applicationContext.getBean("jdbcTemplateTool",JdbcTemplateTool.class);
 		
 		List<Object[]> params = new ArrayList<Object[]>();
@@ -72,14 +85,14 @@ public class JdbcTemplateToolTest extends AbstractJUnit4SpringContextTests{
 		ScaffoldUnit.dbAssertThat("select age from employee where name='tim'", is(29));
 	}
 
-	@Test
-	public void testSave() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDelete() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testSave() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDelete() {
+//		fail("Not yet implemented");
+//	}
 
 }
